58c7cf2aeacd218896b053a4d17b4da78a7f1f0e
slava <vyacheslavkolodrubskiy@gmail.com>
2023-05-17 17:34:44 +0600

    chore: update

--- src/composables/useAxios.ts
+++ src/composables/useAxios.ts
@@ -1,9 +1,6 @@
-import { api } from 'boot/axios'
-
 export function useAxios() {
   async function request(url: string, config?: {}): Promise<any> {
     try {
-      await api(url, config)
     } catch (error) {
       console.error(error)
     }
