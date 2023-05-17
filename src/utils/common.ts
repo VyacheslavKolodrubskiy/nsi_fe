aeb867d1e06cc9b74424a82202c4e71996ce1493
slava <vyacheslavkolodrubskiy@gmail.com>
2023-05-17 16:05:53 +0600

    chore: update

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,9 +1,14 @@
 export function formatPhoneNumber(phoneNumber: string): string {
-  const cleaned = phoneNumber.replace(/\D/g, '')
-  const match = cleaned.match(/^(\d{1})(\d{3})(\d{3})(\d{3})$/)
+  const cleaned: string = phoneNumber.replace(/^\D+/g, '')
+
+  const match: RegExpMatchArray | null = cleaned.match(
+    /^(\d{1})(\d{3})(\d{3})(\d{3})$/
+  )
 
   if (match) {
-    return `+${match[1]} (${match[2]})-${match[3]}-${match[4]}`
+    const [_, countryCode, areaCode, firstThree, lastFour] = match
+
+    return `+${countryCode} (${areaCode})-${firstThree}-${lastFour}`
   }
 
   return phoneNumber
