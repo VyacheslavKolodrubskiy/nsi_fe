00f5a9c0c3b49ceca91d81d355f36d8fcfb7326e
slava <vyacheslavkolodrubskiy@gmail.com>
2023-05-29 23:52:08 +0600

    1

---
+++ src/shared/components/BaseBreadcrumbs.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { PageName } from 'shared/enums/common'
+
+const breadcrumbs = [
+  {
+    label: 'Главная',
+    to: { name: PageName.DEFAULT },
+  },
+  {
+    label: 'Личный кабинет',
+    to: { name: PageName.DEFAULT },
+  },
+]
+</script>
+
+<template>
+  <QBreadcrumbs
+    active-color="grey"
+    class="q-mb-lg"
+  >
+    <template #separator>
+      <svg
+        class="flex"
+        fill="none"
+        height="4"
+        viewBox="0 0 4 4"
+        width="4"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          fill="#7F8A94"
+          height="4"
+          rx="2"
+          width="4"
+        />
+      </svg>
+    </template>
+
+    <QBreadcrumbsEl
+      v-for="breadcrumb in breadcrumbs"
+      :key="breadcrumb.label"
+      :label="breadcrumb.label"
+      :to="breadcrumb.to"
+    />
+  </QBreadcrumbs>
+</template>
