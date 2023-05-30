acf98a465822f0460c064a27f04608431cf2a2d1
slava <vyacheslavkolodrubskiy@gmail.com>
2023-05-30 23:21:00 +0600

    1

--- src/shared/components/TheHeader.vue
+++ src/shared/components/TheHeader.vue
@@ -4,13 +4,29 @@ import { PageName } from 'shared/enums/common'
 
 <template>
   <QHeader
-    class="bg-white q-py-md"
+    class="bg-white q-py-md q-px-xl"
     style="border-radius: 0px 0px 10px 10px"
   >
     <QToolbar>
-      <RouterLink :to="{ name: PageName.DASHBOARD_MAIN }">
-        <LogoIcon />
-      </RouterLink>
+      <div class="row items-center">
+        <RouterLink
+          class="q-mr-md"
+          :to="{ name: PageName.DASHBOARD_MAIN }"
+        >
+          <LogoIcon />
+        </RouterLink>
+
+        <div class="row items-center">
+          <div
+            class="q-mr-md"
+            style="color: #7f8a94"
+          >
+            Қазақ
+          </div>
+
+          <div>Русский</div>
+        </div>
+      </div>
 
       <QSpace />
 
