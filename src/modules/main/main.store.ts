49c45b718dda5c76cefb2e3c2f49b8ad9004fbe3
slava <vyacheslavkolodrubskiy@gmail.com>
2023-05-31 21:28:05 +0600

    1

--- src/modules/main/main.store.ts
+++ src/modules/main/main.store.ts
@@ -1,43 +1,10 @@
 import { defineStore } from 'pinia'
 
-interface AuthState {
-  token: string | null
-}
+interface MainState {}
 
-export const useAuthStore = defineStore('auth', {
-  state: (): AuthState => ({
-    token: null,
-  }),
-  getters: {
-    isAuthenticated: (state) => !!state.token,
-  },
-  actions: {
-    async sendValidatingCode(phone: string) {
-      try {
-        await this.$api.post('/profile/send_validating_code', {
-          err_ne: false,
-          phone,
-        })
-      } catch (error) {
-        console.error(error)
-      }
-    },
-    async auth(phone: string, sms_code: string) {
-      try {
-        await this.$api.post('/profile/auth', {
-          sms_code,
-          phone,
-        })
-      } catch (error) {
-        console.error(error)
-      }
-    },
-    setToken(token: string) {
-      this.token = token
-    },
-    clearToken() {
-      this.token = null
-    },
-  },
+export const useAuthStore = defineStore('main', {
+  state: (): MainState => ({}),
+  getters: {},
+  actions: {},
   persist: true,
 })
