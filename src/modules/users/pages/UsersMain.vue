c514e9f9c76b62960adcf54195609ab7db47523d
slava <vyacheslavkolodrubskiy@gmail.com>
2023-05-31 16:09:32 +0600

    1

--- src/modules/users/pages/UsersMain.vue
+++ src/modules/users/pages/UsersMain.vue
@@ -10,166 +10,90 @@ function createNewUser() {
 const columns: QTableColumn[] = [
   {
     name: 'name',
-    required: true,
-    label: 'Dessert (100g serving)',
     align: 'left',
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
+    label: 'ФИО',
+    field: 'name',
+    headerStyle: 'color: #7F8A94',
   },
   {
-    name: 'calories',
-    align: 'center',
-    label: 'Calories',
-    field: 'calories',
-    sortable: true,
-  },
-  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-  { name: 'protein', label: 'Protein (g)', field: 'protein' },
-  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-  {
-    name: 'calcium',
-    label: 'Calcium (%)',
-    field: 'calcium',
-    sortable: true,
-    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+    name: 'email',
+    align: 'left',
+    label: 'Почта/ Логин',
+    field: 'email',
+    headerStyle: 'color: #7F8A94',
   },
   {
-    name: 'iron',
-    label: 'Iron (%)',
-    field: 'iron',
-    sortable: true,
-    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+    name: 'role',
+    align: 'left',
+    label: 'Роль',
+    field: 'role',
+    headerStyle: 'color: #7F8A94',
+  },
+  {
+    name: 'lastLoginTime',
+    align: 'left',
+    label: 'Время последнего входа',
+    field: 'lastLoginTime',
+    headerStyle: 'color: #7F8A94',
+  },
+  {
+    name: 'status',
+    align: 'left',
+    label: 'Статус',
+    field: 'status',
+    headerStyle: 'color: #7F8A94',
   },
 ]
 
 const rows = [
   {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87,
-    calcium: '14%',
-    iron: '1%',
+    name: 'Марина Кравец',
+    email: 'm.kravets@machta.kz',
+    role: 'Контент-менеджер',
+    lastLoginTime: '12:34  |  04.04.2023',
+    status: 'Активен',
   },
   {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129,
-    calcium: '8%',
-    iron: '1%',
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    sodium: 337,
-    calcium: '6%',
-    iron: '7%',
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    sodium: 413,
-    calcium: '3%',
-    iron: '8%',
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    sodium: 327,
-    calcium: '7%',
-    iron: '16%',
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    sodium: 50,
-    calcium: '0%',
-    iron: '0%',
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    sodium: 38,
-    calcium: '0%',
-    iron: '2%',
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    sodium: 562,
-    calcium: '0%',
-    iron: '45%',
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    sodium: 326,
-    calcium: '2%',
-    iron: '22%',
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    sodium: 54,
-    calcium: '12%',
-    iron: '6%',
+    name: 'Камар',
+    email: 'm.kravets@machta2.kz',
+    role: 'Контент-менеджер2',
+    lastLoginTime: '12:34  |  04.04.2023',
+    status: 'Активен',
   },
 ]
 </script>
 
 <template>
   <QBtn
-    class="q-mb-md q-pa-md"
+    class="q-mb-lg q-pa-md"
     color="primary"
     icon="plus"
     label="Создать нового пользователя"
     @click="createNewUser"
   />
 
-  <QTable
-    :columns="columns"
-    :filter="filter"
-    flat
-    row-key="name"
-    :rows="rows"
+  <div
+    class="q-py-lg q-px-xl bg-white"
+    style="border-radius: 10px"
   >
-    <template #header>
-      <QCard flat>
-        <QCardSection>
-          <div>Список пользователей</div>
+    <QTable
+      :columns="columns"
+      :filter="filter"
+      flat
+      row-key="name"
+      :rows="rows"
+    >
+      <template #top-left>
+        <div>
+          <div class="q-mb-md">
+            <div style="font-size: 20px; color: #39444e">
+              Список пользователей
+            </div>
 
-          <div>Все пользователи Мастер-каталога</div>
+            <div style="font-size: 13px; color: #7f8a94">
+              Все пользователи Мастер-каталога
+            </div>
+          </div>
 
           <QInput
             v-model="filter"
@@ -180,13 +104,17 @@ const rows = [
             type="text"
           >
             <template #append>
-              <SvgIcon name="search" />
+              <SvgIcon
+                height="24"
+                name="search"
+                width="24"
+              />
             </template>
           </QInput>
-        </QCardSection>
-      </QCard>
-    </template>
-  </QTable>
+        </div>
+      </template>
+    </QTable>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
