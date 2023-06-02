68fbd69fd34872ee037e073b1958e680b27f6181
slava <vyacheslavkolodrubskiy@gmail.com>
2023-06-02 23:02:52 +0600

    1

--- src/modules/auth/pages/AuthMain.vue
+++ src/modules/auth/pages/AuthMain.vue
@@ -4,23 +4,8 @@ import authProfile from 'assets/img/auth-profile.png'
 </script>
 
 <template>
-  <QPage class="bg-color" padding style="z-index: 1">
-    <SvgIcon
-      class="q-mx-auto block q-mt-md"
-      height="24"
-      name="logo"
-      width="160"
-    />
-
-    <SvgIcon
-      class="absolute-bottom-left"
-      height="180"
-      name="auth-decor"
-      style="z-index: -1"
-      width="320"
-    />
-
-    <div class="row no-wrap window-height">
+  <QPage class="column" padding>
+    <div class="row no-wrap items-center justify-center col">
       <div class="col flex flex-center q-px-md">
         <AuthForm />
       </div>
@@ -34,15 +19,5 @@ import authProfile from 'assets/img/auth-profile.png'
         />
       </div>
     </div>
-
-    <div class="text-center" style="font-size: 13px; color: #7f8a94">
-      © 2023 Mechta. Все права защищены.
-    </div>
   </QPage>
 </template>
-
-<style scoped lang="scss">
-.bg-color {
-  background-image: linear-gradient(to right, #f2f3f3 50%, #e7e9e9 50%);
-}
-</style>
--- src/shared/layouts/TheAuthLayout.vue
+++ src/shared/layouts/TheAuthLayout.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts"></script>
 
 <template>
-  <QLayout view="hHr LpR lFf">
+  <QLayout class="bg-color" style="z-index: 1" view="hHr LpR lFf">
+    <SvgIcon
+      class="absolute-bottom-left"
+      height="180"
+      name="auth-decor"
+      style="z-index: -1"
+      width="320"
+    />
+
+    <QHeader class="transparent">
+      <QToolbar>
+        <QToolbarTitle>
+          <SvgIcon
+            class="q-mx-auto block q-mt-md"
+            height="24"
+            name="logo"
+            width="160"
+          />
+        </QToolbarTitle>
+      </QToolbar>
+    </QHeader>
+
     <QPageContainer>
       <RouterView />
     </QPageContainer>
+
+    <QFooter class="transparent">
+      <QToolbar>
+        <QToolbarTitle
+          class="text-center"
+          style="font-size: 13px; color: #7f8a94"
+        >
+          © 2023 Mechta. Все права защищены.
+        </QToolbarTitle>
+      </QToolbar>
+    </QFooter>
   </QLayout>
 </template>
+
+<style lang="scss">
+.bg-color {
+  background-image: linear-gradient(to right, #f2f3f3 50%, #e7e9e9 50%);
+}
+</style>
