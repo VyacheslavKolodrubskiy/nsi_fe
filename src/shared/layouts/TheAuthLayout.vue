53371beebc8b6880dceaafcc0235c6722cbe1ec2
slava <vyacheslavkolodrubskiy@gmail.com>
2023-06-03 22:50:57 +0600

    1

--- src/shared/layouts/TheAuthLayout.vue
+++ src/shared/layouts/TheAuthLayout.vue
@@ -6,7 +6,7 @@
       background-image: linear-gradient(to right, #f2f3f3 50%, #e7e9e9 50%);
       z-index: 1;
     "
-    view="hHr LpR lFf"
+    view="hhh lpR fff"
   >
     <SvgIcon
       class="absolute-bottom-left"
