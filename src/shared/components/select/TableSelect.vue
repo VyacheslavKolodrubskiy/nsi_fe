5dc39df77e3046aadc309b321a4710240237fa36
slava <vyacheslavkolodrubskiy@gmail.com>
2023-06-15 21:05:18 +0600

    1

--- src/shared/components/select/TableSelect.vue
+++ src/shared/components/select/TableSelect.vue
@@ -4,23 +4,36 @@ import { QSelectProps, QSelectSlots } from 'quasar'
 interface Props extends QSelectProps {}
 
 const props = withDefaults(defineProps<Props>(), {
-  dropdownIcon: 'expand_more',
   bgColor: 'color-4',
 })
 
 const emit = defineEmits(['update:model-value'])
+
+const expanded = ref(false)
 </script>
 
 <template>
   <QSelect
     :bg-color="bgColor"
     dense
-    :dropdown-icon="dropdownIcon"
+    hide-dropdown-icon
     :model-value="modelValue"
     :options="options"
     outlined
+    @popup-hide="expanded = false"
+    @popup-show="expanded = true"
     @update:model-value="emit('update:model-value', $event)"
   >
+    <template #append>
+      <QIcon
+        :class="{ 'rotate-180 ': expanded }"
+        color="color-1"
+        name="expand_more"
+        size="sm"
+        style="transition: 0.3s"
+      />
+    </template>
+
     <template
       v-for="(_, name) in ($slots as Readonly<QSelectSlots>)"
       :key="name"
